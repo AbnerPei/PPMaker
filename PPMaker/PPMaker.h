205f702753efc65b24f48ccaf688ca45ee320f34
fei <fei@fffffei.com>
2019-04-18 08:32:21 +0800

    update UIDevice define

--- PPMaker/PPMaker.h
+++ PPMaker/PPMaker.h
@@ -35,6 +35,31 @@ CG_INLINE CGFloat UIScreenHeight(){
     return [UIScreen mainScreen].bounds.size.height;
 }
 
+CG_INLINE BOOL isiPhone(){
+    return [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone;
+}
+
+CG_INLINE BOOL isiPhoneXSeries(){
+    return (UIScreenHeight() == 812) || (UIScreenHeight() == 896);
+}
+
+CG_INLINE CGFloat statusBarHeight(){
+    return isiPhoneXSeries() ? 44 : 20;
+}
+
+CG_INLINE CGFloat navgationBarHeight(){
+    return isiPhoneXSeries() ? 88 : 64;
+}
+
+CG_INLINE CGFloat safeBottomHeight(){
+    return isiPhoneXSeries() ? 34 : 0.0;
+}
+
+CG_INLINE BOOL iosVersion(CGFloat version){
+    return [[[UIDevice currentDevice] systemVersion] floatValue] >= version ? YES : NO;
+}
+
+
 NS_ASSUME_NONNULL_END
 
 

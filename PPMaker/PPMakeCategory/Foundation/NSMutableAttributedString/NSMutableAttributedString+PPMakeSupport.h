529bf19373ac27aab26d8a035e8b9a7d14513bd6
PPAbner <chinesemanbobo@163.com>
2021-01-08 09:18:03 +0800

    修改NSDate&NSMutibledString的Category

--- PPMaker/PPMakeCategory/Foundation/NSMutableAttributedString/NSMutableAttributedString+PPMakeSupport.h
+++ PPMaker/PPMakeCategory/Foundation/NSMutableAttributedString/NSMutableAttributedString+PPMakeSupport.h
@@ -7,9 +7,46 @@
 //
 
 #import <Foundation/Foundation.h>
-@class PPMutAttributedStringMaker;
 
 NS_ASSUME_NONNULL_BEGIN
+
+@interface PPMutAttributedStringMaker : NSObject
+
+/** 字体 */
+@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^font)(UIFont *font);
+@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^fontRange)(UIFont *font,NSRange range);
+
+/** 文字颜色 */
+@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^textColor)(UIColor *textColor);
+@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^textColorRange)(UIColor *textColor,NSRange range);
+
+/** 段落 */
+@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^paragraphStyle)(NSParagraphStyle *paragraphStyle);
+@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^paragraphStyleRange)(NSParagraphStyle *paragraphStyle,NSRange range);
+
+//行间距(竖直)
+@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^lineSpacing)(CGFloat lineSpacing);
+@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^lineSpacingRange)(CGFloat lineSpacing,NSRange range);
+
+//字间距(横向) 次数注意参数类型为NSNumber.
+@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^kern)(NSNumber *kern);
+@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^kernRange)(NSNumber *kern,NSRange range);
+
+/**
+ *【only one】某个指定的特别字符串处理
+ * ☠☠☠ 注意: 就算有多个相同的字符串，也只处理第一个☠☠☠
+ */
+@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^specialText)(NSString *specialText,UIFont *specialFont,UIColor *specialColor);
+
+/**
+ *【one or more】某些指定的字符串快速处理集合, ☆☆ 数组个数最好相同 ☆☆ 。
+ */
+@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^specialTextSet)(NSArray<NSString *> *specialTextArray,NSArray<UIFont *> *specialTextFontArray,NSArray<UIColor *> *specialTextColorArray);
+
+@end
+
+
+
 
 @interface NSMutableAttributedString (PPMakeSupport)
 
@@ -80,39 +117,6 @@ NS_ASSUME_NONNULL_BEGIN
 @end
 
 
-@interface PPMutAttributedStringMaker : NSObject
 
-/** 字体 */
-@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^font)(UIFont *font);
-@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^fontRange)(UIFont *font,NSRange range);
-
-/** 文字颜色 */
-@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^textColor)(UIColor *textColor);
-@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^textColorRange)(UIColor *textColor,NSRange range);
-
-/** 段落 */
-@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^paragraphStyle)(NSParagraphStyle *paragraphStyle);
-@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^paragraphStyleRange)(NSParagraphStyle *paragraphStyle,NSRange range);
-
-//行间距(竖直)
-@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^lineSpacing)(CGFloat lineSpacing);
-@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^lineSpacingRange)(CGFloat lineSpacing,NSRange range);
-
-//字间距(横向) 次数注意参数类型为NSNumber.
-@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^kern)(NSNumber *kern);
-@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^kernRange)(NSNumber *kern,NSRange range);
-
-/**
- *【only one】某个指定的特别字符串处理
- * ☠☠☠ 注意: 就算有多个相同的字符串，也只处理第一个☠☠☠
- */
-@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^specialText)(NSString *specialText,UIFont *specialFont,UIColor *specialColor);
-
-/**
- *【one or more】某些指定的字符串快速处理集合, ☆☆ 数组个数最好相同 ☆☆ 。
- */
-@property (nonatomic, copy, readonly) PPMutAttributedStringMaker *(^specialTextSet)(NSArray<NSString *> *specialTextArray,NSArray<UIFont *> *specialTextFontArray,NSArray<UIColor *> *specialTextColorArray);
-
-@end
 
 NS_ASSUME_NONNULL_END
